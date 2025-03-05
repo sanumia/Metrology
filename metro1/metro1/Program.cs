19a1123b9eb4aec7e6109d9a300e109c889294a4
sanumia <diana21032006w@gmail.com>
2025-03-05 10:56:08 +0300

    metro lr1

---
+++ metro1/metro1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Irony.Parsing;
+using metro1;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+class Proram()
+{
+   
+    static void Main()
+    {
+        var scalaCode = "def add(x) { x + 1 }"; // Пример Scala-кода
+
+        var metricCalculator = new MetricCalculator();
+        var (operators, operands) = metricCalculator.GetMetrics(scalaCode);
+
+        Console.WriteLine($"Total Operators: {operators}");
+        Console.WriteLine($"Total Operands: {operands}");
+    }
+
+}
