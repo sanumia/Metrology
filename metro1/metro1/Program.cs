2f19b031e2bbbacd6c8397aad54c1ca293e278ab
LastJayG <146539704+LastJayG@users.noreply.github.com>
2025-03-05 11:54:27 +0300

    added front

--- metro1/metro1/Program.cs
+++ metro1/metro1/Program.cs
@@ -3,12 +3,12 @@ using metro1;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
-class Proram()
+class Program()
 {
    
     static void Main()
     {
-        var scalaCode = "def add(x) { x + 1 }"; // Пример Scala-кода
+        var scalaCode = "def mainLoop(): Unit = {\r\n    displayWelcome()\r\n    \r\n    var playAgain = true\r\n    while (playAgain) {\r\n      val config = chooseDifficulty()\r\n      playGame(config)\r\n      playAgain = askForReplay()\r\n    }\r\n    \r\n    coloredPrint(\"\\nСпасибо за игру! До встречи!\", ANSI_GREEN)\r\n  }"; // Пример Scala-кода
 
         var metricCalculator = new MetricCalculator();
         var (operators, operands) = metricCalculator.GetMetrics(scalaCode);
