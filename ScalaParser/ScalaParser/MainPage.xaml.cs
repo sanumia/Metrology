ab0a21a2d53999ce6177177c040f0c0cd02600a4
LastJayG <146539704+LastJayG@users.noreply.github.com>
2025-03-05 16:31:31 +0300

    ScalaParser instead Parser

---
+++ ScalaParser/ScalaParser/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+
+namespace ScalaParser
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnParseButtonClicked(object sender, EventArgs e)
+        {
+            string? code = TextBox.Text;
+            TableLabel.Text = TextBox.Text;
+        }
+        void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //
+        }
+    }
+
+}
---
+++ ScalaParser/ScalaParser/ParsingService/MetricCalculator.cs
@@ -0,0 +1,46 @@
+﻿using Irony.Parsing;
+
+namespace ScalaParser
+{
+
+    public class MetricCalculator
+    {
+        private int totalOperators;
+        private int totalOperands;
+
+        public void CountMetrics(ParseTreeNode node)
+        {
+            if (node.Term != null)
+            {
+                if (node.Term.Name == "operator")
+                    totalOperators++;
+                else if (node.Term.Name == "operand")
+                    totalOperands++;
+            }
+
+            foreach (var child in node.ChildNodes)
+            {
+                CountMetrics(child);
+            }
+        }
+
+        public (int operators, int operands) GetMetrics(string scalaCode)
+        {
+            var grammar = new ScalaGrammar();
+            var parser = new Parser(grammar);
+            var parseTree = parser.Parse(scalaCode);
+
+            if (parseTree.HasErrors())
+            {
+                foreach (var error in parseTree.ParserMessages)
+                {
+                    Console.WriteLine(error.Message);
+                }
+                return (0, 0);
+            }
+
+            CountMetrics(parseTree.Root);
+            return (totalOperators, totalOperands);
+        }
+    }
+}
